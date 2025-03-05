53853f7ef8f6652d8dcf16fe5a563d401dfc05cd
zerock <kangckck24@gmail.com>
2025-03-05 11:54:37 +0900

    2nd

--- src/main.tsx
+++ src/main.tsx
@@ -4,13 +4,21 @@ import './index.css'
 
 import {BrowserRouter, Route, Routes} from "react-router";
 import MainPage from "./pages/mainPage.tsx";
-import AboutPage from "./pages/aboutPage.tsx";
+import {lazy, Suspense} from "react";
+
+const About =
+    lazy(() => import("./pages/aboutPage"))
+
+const Loading = "<div>Loading..............</div>"
 
 createRoot(document.getElementById('root')!).render(
   <BrowserRouter>
       <Routes>
-          <Route path="/" element={<MainPage />} />
-          <Route path="/about" element={<AboutPage />} />
+          <Route path="/"
+                 element={<MainPage />} />
+          <Route path="/about"
+                 element = {<Suspense fallback={Loading}><About/></Suspense>} />
       </Routes>
   </BrowserRouter>,
 )
+
