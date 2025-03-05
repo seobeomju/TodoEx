7b9c786376d26d79c59e904a832ceb2194f55102
zerock <kangckck24@gmail.com>
2025-03-05 13:20:22 +0900

    2nd

--- src/main.tsx
+++ src/main.tsx
@@ -2,22 +2,13 @@
 import { createRoot } from 'react-dom/client'
 import './index.css'
 
-import {BrowserRouter, Route, Routes} from "react-router";
-import MainPage from "./pages/mainPage.tsx";
-import {lazy, Suspense} from "react";
-
-const About =
-    lazy(() => import("./pages/aboutPage"))
-
-const Loading = "<div>Loading..............</div>"
+import {BrowserRouter,  Routes} from "react-router";
+import rootRouter from "./router/root.tsx";
 
 createRoot(document.getElementById('root')!).render(
   <BrowserRouter>
       <Routes>
-          <Route path="/"
-                 element={<MainPage />} />
-          <Route path="/about"
-                 element = {<Suspense fallback={Loading}><About/></Suspense>} />
+          {...rootRouter()}
       </Routes>
   </BrowserRouter>,
 )
---
+++ src/router/root.tsx
@@ -0,0 +1,21 @@
+import {Route} from "react-router";
+import {lazy, Suspense} from "react";
+
+
+const Loading = "<div>Loading..............</div>"
+
+const Main =
+    lazy(() => import("../pages/mainPage"))
+
+const About =
+    lazy(() => import("../pages/aboutPage"))
+
+
+export default function rootRouter() {
+
+    return [
+        <Route path={'/'} element={ <Suspense fallback={Loading}><Main/></Suspense> } ></Route>,
+        <Route path={'/about'} element={ <Suspense fallback={Loading}><About/></Suspense> } ></Route>
+    ]
+
+}
