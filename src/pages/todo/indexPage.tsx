90d5fd89069d29d237f80d0539cb61994b1916aa
zerock <kangckck24@gmail.com>
2025-03-05 14:30:39 +0900

    2nd

--- src/pages/todo/indexPage.tsx
+++ src/pages/todo/indexPage.tsx
@@ -1,15 +1,15 @@
 import CustomLayout from "../../layouts/customLayout.tsx";
-import {Outlet} from "react-router";
+import {NavLink, Outlet} from "react-router";
 
 function TodoIndexPage() {
     return (
         <CustomLayout>
             <div className="flex gap-4 p-4">
-                <div className="flex-1 bg-white p-4 shadow rounded">
-                    LIST
+                <div className="flex-1 bg-gray-300 p-4 shadow rounded bold">
+                    <NavLink to={'/todo/list'}>LIST</NavLink>
                 </div>
-                <div className="flex-1 bg-white p-4 shadow rounded">
-                    ADD
+                <div className="flex-1 bg-gray-300 p-4 shadow rounded bold ">
+                    <NavLink to={'/todo/add'}>ADD</NavLink>
                 </div>
             </div>
             <div>
