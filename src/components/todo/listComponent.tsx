8271b4c077c5375769fb84ffb47e03a87dc908d4
zerock <kangckck24@gmail.com>
2025-03-05 14:36:34 +0900

    2nd

---
+++ src/components/todo/listComponent.tsx
@@ -0,0 +1,14 @@
+
+function ListComponent() {
+
+    return (
+        <div className="border-2 border-blue-100 mt-10 mr-2 ml-2">
+
+            <div className="flex flex-wrap mx-auto justify-center p-6">
+                List Component
+            </div>
+        </div>
+    );
+}
+
+export default ListComponent;
