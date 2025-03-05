93976b6dad3955c28761b07150ad2215107602a7
zerock <kangckck24@gmail.com>
2025-03-05 14:37:30 +0900

    2nd

--- src/pages/todo/listPage.tsx
+++ src/pages/todo/listPage.tsx
@@ -1,8 +1,12 @@
+import ListComponent from "../../components/todo/listComponent.tsx";
 
 function ListPage() {
     return (
         <div className={'mt-3 p-3 bg-blue-200 w-full h-full'}>
             <div>Todo List</div>
+            <div>
+                <ListComponent></ListComponent>
+            </div>
         </div>
     );
 }
