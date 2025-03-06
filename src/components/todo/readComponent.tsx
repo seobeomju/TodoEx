b918d7072c84671e04eeff51af23cd293fe08c6b
zerock <kangckck24@gmail.com>
2025-03-06 11:16:36 +0900

    2nd

--- src/components/todo/readComponent.tsx
+++ src/components/todo/readComponent.tsx
@@ -1,5 +1,29 @@
+import {useParams} from "react-router";
+import {useEffect, useState} from "react";
+import {getTodo} from "../../api/todoApi.tsx";
+
+const initState:Todo = {
+    tno:0,
+    title:'',
+    writer:'',
+    regDate:'',
+    modDate:''
+}
 
 function ReadComponent() {
+
+    const [todo, setTodo] = useState<Todo>(initState)
+
+    const {tno} = useParams()
+
+    useEffect(() => {
+
+        getTodo(tno).then(data => {
+            setTodo(data)
+        })
+
+    },[tno])
+
     return (
         <div>
             <div>Todo Read Component</div>
