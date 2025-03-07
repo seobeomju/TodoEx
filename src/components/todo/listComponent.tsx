a17ea24a6149660c81ea24df6b5183146389c4a2
seobeomju <88228101+seobeomju@users.noreply.github.com>
2025-03-07 09:48:56 +0900

    페이지 이동 및 리스트 컴포넌트

--- src/components/todo/listComponent.tsx
+++ src/components/todo/listComponent.tsx
@@ -1,17 +1,30 @@
+import useCustomParam from "../../hooks/useCustomParam.tsx";
+import {useEffect, useState} from "react";
+import {getTodoList} from "../../api/todoApi.tsx";
+import PageComponent from "../common/pageComponent.tsx";
 
+const initState:PageResponse<Todo> = {
+    dtoList: [],
+    total:0,
+    size: 0,
+    end : 0,
+    next: false,
+    prev: false,
+    page: 0,
+    start: 0
+}
 
-// const initState:PageResponse<Todo> = {
-//     dtoList: [],
-//     total:0,
-//     size: 0,
-//     end : 0,
-//     next: false,
-//     prev: false,
-//     page: 0,
-//     start: 0
-// }
 
 function ListComponent() {
+
+    const{page, size,movePage} = useCustomParam()
+
+    const [serverData, setServerData]= useState<PageResponse<Todo>>(initState)
+    useEffect(()=>{
+        getTodoList(page, size).then(data => {
+            setServerData(data)
+        });
+    },[page,size])
 
     return (
         <div className="border-2 border-blue-100 mt-10 mr-2 ml-2">
@@ -22,8 +35,18 @@ function ListComponent() {
 
             <div>
                 <ul className="w-full max-w-2xl mx-auto bg-white shadow-lg rounded-lg overflow-hidden m-2">
+
+                    {serverData.dtoList.map(todo =>
+                        <li key={todo.tno}>
+                            {todo.tno} - {todo.title}
+                        </li>
+                    )}
+
+
                 </ul>
             </div>
+
+            <PageComponent serverData={serverData} moveListPage={movePage}/>
         </div>
     );
 }
--- src/hooks/useCustomParam.tsx
+++ src/hooks/useCustomParam.tsx
@@ -1,4 +1,4 @@
-import { useSearchParams} from "react-router";
+import {useNavigate, useSearchParams} from "react-router";
 import {useState} from "react";
 
 function getNumber(str : string|null, defaultValue:number):number {
@@ -19,13 +19,18 @@ export default function useCustomParam(){
 
     //const params = useParams()
     const [searchParams] = useSearchParams()
+    const navigate = useNavigate()
 
     const page:number = getNumber(searchParams.get("page"),1)
 
     const size:number = getNumber(searchParams.get("size"),10)
 
     const [refresh, setRefresh] = useState(false)
+    const movePage = (pageNum:number):void=>{
 
-    return {page,size,refresh,setRefresh}
+        navigate(`/todo/list?page=${pageNum}&size=${size}`)
+    }
+
+    return {page,size,refresh,setRefresh, movePage}
 
 }
